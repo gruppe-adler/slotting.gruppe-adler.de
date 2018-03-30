9b9714714a834fe6f97d0cffc56058e46ce49e79
Christian Klemm <chris579@web.de>
2018-03-30 19:31:20 +0200

    profile picture fix

--- src/app/slotting/slot.component.ts
+++ src/app/slotting/slot.component.ts
@@ -70,7 +70,7 @@ export class SlotComponent implements OnInit {
    */
   private parseProfilePicture(): void {
     if (this.slot.user && this.slot.user.picture && !this.slot.user.picture.startsWith('http')) {
-      this.slot.user.picture = '//forum.gruppe-adler.de' + this.slot.user.picture;
+      this.slot.user.picture = 'https://forum.gruppe-adler.de' + this.slot.user.picture;
     }
   }
 
