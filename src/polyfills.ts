f54e9fc986f44eae99c038c1f4577265973f914b
Moritz Schmidt <mo.schmidt@rebuy.com>
2018-07-13 17:29:32 +0200

    tentative Blob polyfill fix

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -35,7 +35,7 @@ import 'core-js/es6/weak-map';
 import 'core-js/es6/set';
 /** IE9 Http Polyfills */
 import 'js-polyfills/typedarray';
-import 'blob.js/blob';
+import 'blob.js/Blob';
 import 'formdata-polyfill/FormData';
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 import 'classlist.js'; // Run `npm install --save classlist.js`.
