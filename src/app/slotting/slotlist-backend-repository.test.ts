869e627993748494ddc6dd804af30502557821be
Moritz Schmidt <mo.schmidt@rebuy.com>
2018-08-10 08:39:02 +0200

    library update, test framework change
    	* update to Angular 6 and TS 2.9
    	* remove Jasmine, Karma
    	* use Jest instead

---
+++ src/app/slotting/slotlist-backend-repository.test.ts
@@ -0,0 +1,11 @@
+import 'jest';
+import {ok} from 'assert';
+import {SlotlistBackendRepository} from './slotlist-backend-repository';
+
+
+describe('SlotlistBackendRepository', () => {
+  it('bar', () => {
+    const foo = new SlotlistBackendRepository();
+    ok(true);
+  });
+});
---
+++ src/app/slotting/slotlist-backend-repository.ts
@@ -0,0 +1,7 @@
+export class SlotlistBackendRepository {
+  public constructor() {
+  }
+
+  public save(match: any): any {
+  }
+}
