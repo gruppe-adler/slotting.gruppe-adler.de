7e9bd3a4a15d4524a0dbef876f1dc7742b563592
RednecksRevenge <RednecksRevenge>
2020-06-18 23:42:54 +0200

    rename forumBaseUrl -> forumUrl

--- src/app/slotting/slot.component.ts
+++ src/app/slotting/slot.component.ts
@@ -83,7 +83,7 @@ export class SlotComponent implements OnInit {
    */
   private parseProfilePicture(): void {
     if (this.slot.user && this.slot.user.picture && !this.slot.user.picture.startsWith('http')) {
-      this.slot.user.picture = environment.api.forumBaseUrl + this.slot.user.picture;
+      this.slot.user.picture = environment.api.forumUrl + this.slot.user.picture;
     }
   }
 
